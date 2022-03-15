6ed7c32967ae1fc28e9897b2444ad66354e5038a
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-15 14:04:32 -0400

    added webfont loader

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ import { CanvasManager } from "./utilities/canvasmanager.js";
 const { canvas, c } = CanvasManager.create();
 let sysTime;
 
+WebFont.load({
+  google: {
+    families: ["Teko:300", "Red+Hat+Mono:400"],
+  },
+  timeout: 3000,
+  active: function () {
+    sysTime = Date.now();
+    CanvasManager.setResolution({ canvas, c });
+    window.onresize = () => {
+      CanvasManager.setResolution({ canvas, c });
+    };
+    requestAnimationFrame(loop);
+  },
+});
 
 function loop() {
   let delta = Date.now() - sysTime;
