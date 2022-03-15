1288991c4a04563eb2b002220c4ddb8a24248108
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-15 17:31:21 -0400

    added planet object

---
+++ entities/planets.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    name: "Planet One",
+    density: 1,
+    size: 300,
+    x: 200,
+    y: 300,
+  },
+  {
+    name: "Planet One",
+    density: 1,
+    size: 600,
+    x: 2000,
+    y: 400,
+  },
+  {
+    name: "Planet One",
+    density: 1,
+    size: 400,
+    x: -100,
+    y: 1000,
+  },
+];
