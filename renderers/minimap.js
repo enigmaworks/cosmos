06ff2a362d41c5143c00d3de41baa730009094ca
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-18 12:51:12 -0400

    moved sprite path to entity file, tweaked minmap

--- renderers/minimap.js
+++ renderers/minimap.js
@@ -4,7 +4,7 @@ let firstRender = true;
 let map_elem;
 let map;
 let mapsize;
-const scale = 100;
+const scale = 40;
 export default function ({ camera, planets, player }) {
   if (firstRender) {
     map_elem = document.querySelector(".map");
@@ -30,10 +30,25 @@ export default function ({ camera, planets, player }) {
       map.save();
       map.translate(planet.x - camera.x, planet.y - camera.y);
       map.beginPath();
-      map.fillStyle = "white";
+      map.fillStyle = planet.atmosphere;
       map.arc(0, 0, planet.size, 0, Math.PI * 2);
       map.fill();
       map.restore();
     }
   }
+  map.beginPath();
+  map.fillStyle = "white";
+
+  map.save();
+  map.translate(player.x - camera.x, player.y - camera.y);
+  map.rotate(player.rotation + Math.PI / 2);
+  map.translate((-player.size * scale) / 3, (-player.size * 1.25 * scale) / 3);
+  map.scale(
+    ((0.5 / player.size) * scale) / 3,
+    ((0.5 / player.size) * scale) / 3
+  );
+  map.fillStyle = "#eee";
+  map.fill(new Path2D(player.sprite));
+  map.fill();
+  map.restore();
 }
