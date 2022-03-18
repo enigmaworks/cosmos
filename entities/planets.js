546a804bdfe1d995498d3333d9db7c8353ba3ffb
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-17 21:17:09 -0400

    removed planets again

--- entities/planets.js
+++ entities/planets.js
@@ -1,47 +1,17 @@
 export default [
   {
-    name: "Planet One",
+    name: "Preston's",
     density: 1,
-    size: 300,
-    x: 2000,
-    y: 3000,
-    fill: {
-      type: "gradient",
-      f: {
-        stop1: { s: 0, c: "aqua" },
-        stop2: { s: 1, c: "blue" },
-      },
-    },
-    atmosphere: "aqua",
-  },
-  {
-    name: "Planet One",
-    density: 1,
-    size: 400,
+    size: 450,
     x: 0,
     y: 0,
     fill: {
       type: "gradient",
       f: {
         stop1: { s: 0, c: "red" },
-        stop2: { s: 1, c: "yellow" },
+        stop2: { s: 1, c: "orange" },
       },
     },
-    atmosphere: "red",
-  },
-  {
-    name: "Planet One",
-    density: 1,
-    size: 600,
-    x: -1100,
-    y: 50000,
-    fill: {
-      type: "gradient",
-      f: {
-        stop1: { s: 0, c: "purple" },
-        stop2: { s: 1, c: "magenta" },
-      },
-    },
-    atmosphere: "magenta",
+    atmosphere: "orange",
   },
 ];
