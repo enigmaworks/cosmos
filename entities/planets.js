5dc87981b29f042651339e6db985adc1021b1cd2
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-18 12:36:01 -0400

    added lots of planets

--- entities/planets.js
+++ entities/planets.js
@@ -1,10 +1,9 @@
 export default [
   {
-    name: "Preston's",
     density: 1,
     size: 450,
-    x: 0,
-    y: 0,
+    x: 1340,
+    y: 3040,
     fill: {
       type: "gradient",
       f: {
@@ -13,5 +12,83 @@ export default [
       },
     },
     atmosphere: "orange",
+  },
+  {
+    density: 0.5,
+    size: 650,
+    x: 3000,
+    y: -9000,
+    fill: {
+      type: "gradient",
+      f: {
+        stop1: { s: 0, c: "aqua" },
+        stop2: { s: 1, c: "blue" },
+      },
+    },
+    atmosphere: "aqua",
+  },
+  {
+    density: 2.5,
+    size: 250,
+    x: -6034,
+    y: -4051,
+    fill: {
+      type: "gradient",
+      f: {
+        stop1: { s: 0, c: "salmon" },
+        stop2: { s: 1, c: "pink" },
+      },
+    },
+    atmosphere: "salmon",
+  },
+  {
+    density: 1.15,
+    size: 350,
+    x: -5024,
+    y: 8051,
+    fill: {
+      type: "gradient",
+      f: {
+        stop1: { s: 0, c: "teal" },
+        stop2: { s: 1, c: "aqua" },
+      },
+    },
+    atmosphere: "teal",
+  },
+  {
+    density: 0.8,
+    size: 750,
+    x: -12037,
+    y: 2054,
+    fill: {
+      type: "gradient",
+      f: {
+        stop1: { s: 0, c: "lightgray" },
+        stop2: { s: 1, c: "slategray" },
+      },
+    },
+    atmosphere: "slategray",
+  },
+  {
+    density: 1,
+    size: 400,
+    x: 12037,
+    y: -2054,
+    fill: {
+      type: "color",
+      f: "green",
+    },
+    atmosphere: "green",
+  },
+  {
+    density: 1,
+    size: 500,
+    x: 11037,
+    y: 11054,
+    fill: {
+      type: "color",
+      f: "purple",
+    },
+    atmosphere: "purple",
   },
 ];
