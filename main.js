3bebff20587bf4cd5caa847688ca5ae26546687f
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-03-19 17:40:08 -0400

    moved minimap drag script into main.js

--- main.js
+++ main.js
@@ -22,6 +22,38 @@ WebFont.load({
     document.body.addEventListener("keydown", keyDownHandler);
     document.body.addEventListener("keyup", keyUpHandler);
     startAnimating(60);
+    const map = document.querySelector(".map");
+    map.onmousedown = dragMouseDown;
+    let pos1,
+      pos2,
+      pos3,
+      pos4 = 0;
+    // https://www.w3schools.com/howto/howto_js_draggable.asp
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      map.style.top = map.offsetTop - pos2 + "px";
+      map.style.left = map.offsetLeft - pos1 + "px";
+    }
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   },
 });
 
