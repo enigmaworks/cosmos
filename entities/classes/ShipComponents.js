a0eb0019063aeec7238d62a63445f361a4c688a2
Enigmaworks <82541216+enigmaworks@users.noreply.github.com>
2022-04-05 11:10:46 -0400

    completed ship component classes

--- entities/classes/ShipComponents.js
+++ entities/classes/ShipComponents.js
@@ -1,6 +1,39 @@
 export class ShipComponent {
+  constructor(initial, capacity, { uses = {} }) {
+    this.current = initial;
+    this.max_level = capacity;
+    this.rate = rate;
+    this.uses = {
+      fuel: 0,
+      energy: 0,
+      oxygen: 0,
+      ...uses,
+    };
+  }
 }
 export class StorageItem {
+  constructor(amount, capacity, minimum = 0) {
+    this.amount = initial;
+    this.capacity = capacity;
+    this.minimum_capacity = minimum;
+  }
 }
 export class Weapon {
+  constructor(damage, range, cooldown, { clustersize = 1, cost = {}, amount = Infinity }) {
+    this.damage = damage;
+    this.clustersize = clustersize;
+    this.range = range;
+    this.cooldown = cooldown;
+    this.amount = Infinity;
+    this.amount = amount;
+    this.cost = {
+      uranium: 0,
+      hydrogen: 0,
+      oxygen: 0,
+      aluminum: 0,
+      silver: 0,
+      magnesium: 0,
+      ...cost,
+    };
+  }
 }
